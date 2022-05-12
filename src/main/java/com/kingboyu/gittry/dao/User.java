a447e30b4edb96ba7a95b54ae9d692494a86dbb2
kingboyu <946939322@qq.com>
2022-05-12 14:00:30 +0800

    second commit

--- src/main/java/com/kingboyu/gittry/dao/User.java
+++ src/main/java/com/kingboyu/gittry/dao/User.java
@@ -9,5 +9,7 @@ import org.springframework.boot.autoconfigure.info.ProjectInfoProperties;
 public class User {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,git2");
+
     }
 }
