4a9488450a61cca2ece79d33f279737842fa83d3
kingboyu <946939322@qq.com>
2022-05-12 14:38:55 +0800

    hotfix commit

--- src/main/java/com/kingboyu/gittry/dao/User.java
+++ src/main/java/com/kingboyu/gittry/dao/User.java
@@ -10,6 +10,6 @@ public class User {
     public static void main(String[] args) {
         System.out.println("hello,git");
         System.out.println("hello,git2");
-
+        System.out.println("hello,git3");
     }
 }
